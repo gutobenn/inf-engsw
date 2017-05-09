d575d0ee702057f6614d3bc4713a5a2f42092535
Augusto Bennemann <gutobenn@gmail.com>
2017-05-09 19:33:10 -0300

    translate forms

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,14 +1,16 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Item
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, help_text='Obrigatorio.')
-    last_name = forms.CharField(max_length=30, help_text='Obrigatorio.')
-    email = forms.EmailField(max_length=254, help_text='Obrigatorio. Informe um endereco de email valido.')
+    first_name = forms.CharField(max_length=30, help_text='Obrigatório.', label='Nome')
+    last_name = forms.CharField(max_length=30, help_text='Obrigatório.', label='Sobrenome')
+    email = forms.EmailField(max_length=254, help_text='Obrigatório. Informe um endereço de e-mail válido.')
     phone_number = forms.RegexField(max_length=11, regex=r'^\d{10,11}$',
-                                error_message = ("Numero de telefone precisa estar no formato: 'DDD999999999'. Ate 11 digitos."))
+                                error_message = ("Número de telefone precisa estar no formato: 'DDD999999999'. Até 11 digitos."),
+                                label='Telefone')
 
     class Meta:
         model = User
--- myapp/models.py
+++ myapp/models.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from easymoney import MoneyField
 from django.core.files.storage import FileSystemStorage
 import os
 
 class Item(models.Model):
-    title = models.CharField(max_length=60)
-    description = models.TextField(max_length=800)
+    title = models.CharField(max_length=60, verbose_name='Título')
+    description = models.TextField(max_length=800, verbose_name='Descrição')
     published_date = models.DateTimeField(blank=True, null=True)
     owner = models.ForeignKey('auth.User')
     # image = models.ImageField(upload_to='/your_image_name', storage=upload_storage) TODO add image
-    price = MoneyField(default=0)
+    price = MoneyField(default=0, verbose_name='Preço')
 
     def __str__(self):
         return self.title
