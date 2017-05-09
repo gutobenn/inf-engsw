65fb5eefb8001c3b8a5a45d04c0056c86b04a812
Augusto Bennemann <gutobenn@gmail.com>
2017-05-09 00:36:39 -0300

    User registration, login, logout; menu links when authenticated

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.items, name='items'),
+    url(r'^items/$', views.items, name='items'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'index'}, name='logout'),
+    url(r'^admin/', admin.site.urls),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from myapp.forms import SignUpForm
 
 def index(request):
     return render(request, 'myapp/index.html')
 
 def items(request):
     return render(request, 'myapp/items.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'myapp/signup.html', {'form': form})
